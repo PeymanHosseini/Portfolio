f8efcdf3824227102a839e86b97b30f53a368a13
Peyman Hosseini <peyman.66h@gmail.com>
2019-03-12 14:11:34 +0200

    animation for text added using typed.js

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
     play: 5000,
     pagination: false
   });
+
+  var typed = new Typed(".typed", {
+    strings: ["Web Developer.^800", "Web Designer.^800", "IT Student.^800"],
+    typeSpeed: 80,
+    loop: true,
+    startDelay: 800,
+    showCursor: false
+  });
 });
