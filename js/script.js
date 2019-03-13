49881b41aa8f98a9d8237fffd0cfaa82f73e0fb4
Peyman Hosseini <peyman.66h@gmail.com>
2019-03-13 13:22:10 +0200

    skill section compeleted

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
+  //starting supperslides
   $("#slides").superslides({
     animation: "fade",
     play: 5000,
     pagination: false
   });
-
+  //typed.js animation for text
   var typed = new Typed(".typed", {
     strings: ["Web Developer.^800", "Web Designer.^800", "IT Student.^800"],
     typeSpeed: 80,
@@ -12,23 +13,43 @@ $(document).ready(function() {
     startDelay: 800,
     showCursor: false
   });
+  // owl carousel for skills section
   $(".owl-carousel").owlCarousel({
     loop: true,
-    margin: 10,
-    nav: true,
+    items: 4,
     responsive: {
       0: {
         items: 1
       },
-      600: {
+      480: {
+        items: 2
+      },
+      768: {
         items: 3
       },
-      1000: {
-        items: 5
+      938: {
+        items: 4
       }
     }
   });
-  $(".chart").easyPieChart({
-    //your options goes here
+  // finding top position of skillsSection
+  var skillsTopOffset = $(".skillsSection").offset().top;
+  //when window scroll down this code will start
+  $(window).scroll(function() {
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      $(".chart").easyPieChart({
+        easing: "easeInOut",
+        barColor: "#fff",
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+          $(this.el)
+            .find(".percent")
+            .text(Math.round(percent));
+        }
+      });
+    }
   });
 });
