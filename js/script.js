b9d2222013f8b4524bf774b7b75a6630555e00c5
Peyman Hosseini <peyman.66h@gmail.com>
2019-03-16 18:32:18 +0200

    css for image and button in portfolio section

--- js/script.js
+++ js/script.js
@@ -68,4 +68,13 @@ $(document).ready(function() {
     }
   });
   $("[data-fancybox]").fancybox();
+
+  $(".items").isotope({
+    filter: "*",
+    animationOptions: {
+      duration: 1500,
+      easing: "linear",
+      queue: false
+    }
+  });
 });
