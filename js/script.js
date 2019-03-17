95b25e46e3615efb59a764c9ea41feb5705855f9
Peyman Hosseini <peyman.66h@gmail.com>
2019-03-17 12:22:32 +0200

    sliding to section onclick

--- js/script.js
+++ js/script.js
@@ -92,6 +92,14 @@ $(document).ready(function() {
     });
     return false;
   });
+  $("#navigation li a").click(function(e) {
+    e.preventDefault();
+
+    var targetElement = $(this).attr("href");
+    var targetPosition = $(targetElement).offset().top;
+    $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+  });
+
   const nav = $("#navigation");
   const navTop = nav.offset().top;
 
@@ -101,8 +109,10 @@ $(document).ready(function() {
     var body = $("body");
 
     if ($(window).scrollTop() >= navTop) {
+      body.css("padding-top", nav.outerHeight() + "px");
       body.addClass("fixedNav");
     } else {
+      body.css("padding-top", 0);
       body.removeClass("fixedNav");
     }
   }
